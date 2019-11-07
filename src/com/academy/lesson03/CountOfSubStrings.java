be42f8382f4c5efef0bdf723a3b69a967b41471d
Inna <innasamus@gmail.com>
2019-11-07 22:58:01 +0200

    Substring

---
+++ src/com/academy/lesson03/CountOfSubStrings.java
@@ -0,0 +1,21 @@
+package com.academy.lesson03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class CountOfSubStrings {
+    /*    3) Напишите программу, которая:
+        - считывает строку
+        - считывает подстроку
+        - выводит исходные строку и подстроку и выводит кол-во вхождений строки в подстроку*/
+
+    public static void countOfStrings() {
+        System.out.println("=============================================================================");
+        System.out.println("The program counts the numbers of congruences:");
+        Reader r = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(r);
+
+
+    }
+}
