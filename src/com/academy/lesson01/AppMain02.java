62a144c1f68fa730a1311c947a0ceef8429d0e0c
InnaBagmut <energy777&>
2019-11-07 20:49:49 +0200

    small improvements - 2

--- src/com/academy/lesson01/AppMain02.java
+++ src/com/academy/lesson01/AppMain02.java
@@ -9,3 +9,5 @@ public class AppMain02 {
             System.out.println("Hello!");
     }
 }
+// не ясна логика задачи, и команда java -cp ./out/ com.academy.lesson01.AppMain02 Peter не сработала по инструкции в директории task
+// выполнилась команда java -cp ./out/ com.academy.lesson01.AppMain02 и при запуске jar файла просто вывелось Hello!
