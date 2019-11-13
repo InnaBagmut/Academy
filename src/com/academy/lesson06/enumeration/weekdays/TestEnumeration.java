93d0cb66b1f3e1c4e805bd3b46ffb5128e5ce02e
InnaBagmut <energy777&>
2019-11-13 21:28:09 +0200

    interface - 1

--- src/com/academy/lesson06/enumeration/weekdays/TestEnumeration.java
+++ src/com/academy/lesson06/enumeration/weekdays/TestEnumeration.java
@@ -5,22 +5,24 @@ import static com.academy.lesson06.enumeration.weekdays.WeekDay.*;
 public class TestEnumeration {
     public static void main(String[] args) {
         UseWeekDay myObject = new UseWeekDay();
-        myObject.setToday(MONDAY);
+        myObject.setToday(WEDNESDAY);
         System.out.println(myObject);
         System.out.println(myObject.getToday());
         System.out.println("Day After Tomorrow Option1:");
         myObject.dayAfterTomorrowOption1();
 
-        WeekDay day = SUNDAY;
+
         System.out.println("Day After Tomorrow Option2:");
+        WeekDay day1 = SUNDAY;
         //WEDNESDAY.dayAfterTomorrowOption2();
-        WeekDay dayAfterTomorrow = day.dayAfterTomorrowOption2();
-        System.out.println("2 days after " + day + " is " + dayAfterTomorrow);
+        WeekDay dayAfterTomorrow = day1.dayAfterTomorrowOption2();
+        System.out.println("2 days after " + day1 + " is " + dayAfterTomorrow);
 
         System.out.println("Day Before Yesterday:");
+        WeekDay day2 = MONDAY;
         //TUESDAY.dayBeforeYesterday();
-        WeekDay dayBeforeYesterday = day.dayBeforeYesterday();
-        System.out.println("2 days before " + day + " is " + dayBeforeYesterday);
+        WeekDay dayBeforeYesterday = day2.dayBeforeYesterday();
+        System.out.println("2 days before " + day2 + " is " + dayBeforeYesterday);
 
         System.out.println("This is an array filled by days of the week:");
         WeekDay[] daysArray = {SUNDAY, THURSDAY, MONDAY, FRIDAY, TUESDAY};
