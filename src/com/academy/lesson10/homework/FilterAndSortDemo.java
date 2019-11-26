d44bc776692fa6c33d1ee3050ad6d3d15f4c4fac
InnaBagmut <energy777&>
2019-11-26 13:14:08 +0200

    lesson10 - hw - sorting

--- src/com/academy/lesson10/homework/FilterAndSortDemo.java
+++ src/com/academy/lesson10/homework/FilterAndSortDemo.java
@@ -1,12 +1,20 @@
+/*1) a) Создать List<Date> с разными значениями даты
+        (Date использовать реализовнный ранее).
+        b) Отсортировать даты по возрастанию и вывести список
+        с) Отфильтровать список, оставив только даты с месяцами:
+        январь февраль март
+        d) Выполнить сортировку
+        по годам по убыванию
+        по месяцам по возрастанию
+        по дням по возрастанию
+        => 01.01.2019, 01.01.2018, 01.02.2018, 02.02.2018, 01.01.2017*/
+
 package com.academy.lesson10.homework;
 
 import com.academy.lesson05.date.Date;
 import com.academy.lesson10.homework.ComparatorDateImplementation;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 
 public class FilterAndSortDemo {
@@ -22,12 +30,15 @@ public class FilterAndSortDemo {
         dates.sort(new ComparatorDateImplementation());
         System.out.println(dates);
 
-        System.out.println("С помощью лямбды по дням:");
+        System.out.println("Sorting by Day up due to lambda:");
         dates.sort((o1, o2) -> {
             if (o1.getDay() < o2.getDay()) return -1;
             if (o1.getDay() > o2.getDay()) return 1;
             return 0;
         });
         System.out.println(dates);
+
+        System.out.println("Attempting to filter Collection by month:");
+        Arrays.stream(new List[]{dates}).filter(n -> n.equals(2)).forEach((System.out::println));
     }
 }
